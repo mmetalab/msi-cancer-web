937de830edd165d506b8eff998e9b5b21b76586e
mmetalab <chengwangsdu@outlook.com>
2024-05-18 10:34:18 +0800

    Update spatial_metabolomics_web.py

--- spatial_metabolomics_web.py
+++ spatial_metabolomics_web.py
@@ -50,7 +50,7 @@ server = app.server
 # This will only be executed during debug when run locally, since WSGI does not 
 # run this as __main__ but only takes the "server" variable
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8080)
+    app.run_server(host='0.0.0.0', port=10000)
     # app.run_server(debug=True)
 
 # gunicorn mpivgae:server
